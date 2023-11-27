224815bdc7956a459e787b36e71fb9c5e1280a71
maksonchek <maksprivalov@list.ru>
2023-11-27 19:30:31 +0300

    Добавлена реализация линейной и параболической интерполяций. Тестирование не проводилось, тесты не написаны

--- interpolation/module.py
+++ interpolation/module.py
@@ -19,3 +19,73 @@ def global_interpolation(x_values, y_values, x):
 
     return result
 
+def find_two_nearest_points(x_values, x):
+
+    """
+    Находит ближайшую левую и ближайшую правую точки из массива x_values для x.
+
+    :param x_values: Список значений сеточной функции.
+    :param x: Точка, для которой надо найти ближайшие точки из x_values.
+    :return: (l, l_val, r, r_val) - (индекс в x_values левой ближайшей точки, значение левой ближайшей точки, индекс в x_values правой ближайшей точки, значение правой ближайшей точки)
+    """
+
+    min_r_dist = float('inf')
+    min_l_dist = float('inf')
+
+    l_val = r_val = 0
+    l = r = 0
+    for i, x_i in enumerate(x_values):
+        d = abs(x_i - x)
+        if d < min_l_dist and x_i < x:
+            l_val = x_i
+            min_l_dist = d
+            l = i
+        elif d < min_r_dist and x_i > x:
+            min_r_dist = d
+            r_val = x_i
+            r = i
+    return (l, l_val, r, r_val)
+
+def linear_inperpolation(x_values, y_values, x):
+    """
+    Выполняет линейную интерполяцию. 
+
+    :param x_values: Список значений x (узлов интерполяции).
+    :param y_values: Список значений y (значений функции в узлах интерполяции).
+    :param x: Точка, в которой производится интерполяция.
+    :return: Значение интерполянта в точке x.
+    """
+
+    l, l_val, r, r_val = find_two_nearest_points(x_values, x)
+        
+    result = y_values[l]*(x - l_val)/(l_val - r_val) + y_values[r]*(x - r_val)/(r_val - l_val)
+
+    return result
+
+def parabolic_interpolation(x_values, y_values, x):
+    """
+    Выполняет параболическую интерполяцию.
+
+    :param x_values: Список значений x (узлов интерполяции).
+    :param y_values: Список значений y (значений функции в узлах интерполяции).
+    :param x: Точка, в которой производится интерполяция.
+    :return: Значение интерполянта в точке x.
+    """
+    l_1, l_val_1, r_1, r_val_1 = find_two_nearest_points(x_values, x) #i, i+1
+
+    l_2, l_val_2, _, _ = find_two_nearest_points(x_values, l_val_1) #i-1 
+
+    _, _, r_2, r_val_2 = find_two_nearest_points(x_values, r_val_1) #i+2
+
+    p1 = (x - l_val_1)*(x - r_val_1)/((l_val_2 - l_val_1)*(l_val_2 - r_val_1)) #i-1 (l_val_2)
+    p2 = (x - l_val_2)*(x - r_val_1)/((l_val_1 - l_val_2)*(l_val_1 - r_val_1)) #i (l_val_1)
+    p3 = (x - l_val_1)*(x - r_val_2)/((r_val_1 - l_val_1)*(r_val_1 - r_val_2)) #i+1 (r_val_1)
+    p4 = (x - l_val_2)*(x - r_val_1)/((r_val_2 - l_val_2)*(r_val_2 - r_val_1)) #i+2 (r_val_2)
+
+    l1 = p1*y_values[l_2] + p2*y_values[l_1] + p3*y_values[r_1]
+    l2 = p2*y_values[l_1] + p2*y_values[r_1] + p4*y_values[r_2]
+
+    result = (l1+l2)/2
+
+    return result
+
